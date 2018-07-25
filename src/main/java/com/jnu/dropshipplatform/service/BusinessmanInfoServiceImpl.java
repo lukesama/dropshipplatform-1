607d2e4a998c8dbf0f590a296ea4135dac7ab551
aaa <1169836993@qq.com>
2018-07-25 22:20:20 +0800

    新增了BusinessmanInfo.html和BusinessmanWallet.html界面；新增BusinessmanController

--- src/main/java/com/jnu/dropshipplatform/service/BusinessmanInfoServiceImpl.java
+++ src/main/java/com/jnu/dropshipplatform/service/BusinessmanInfoServiceImpl.java
@@ -1,13 +1,25 @@
 package com.jnu.dropshipplatform.service;
 
+import com.jnu.dropshipplatform.entity.BusinessmanInfo;
 import com.jnu.dropshipplatform.repository.BusinessmanInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BusinessmanInfoServiceImpl implements BusinessmanInfoService {
 
     @Autowired
     private BusinessmanInfoRepository businessmanInfoRepository;
+
+    @Override
+    public BusinessmanInfo getBusiInfoByID(Integer userBusiId){
+        return businessmanInfoRepository.getOne(userBusiId);
+    }
+    @Override
+    public BusinessmanInfo updateBusiInfo(BusinessmanInfo businessmanInfo) {
+        return businessmanInfoRepository.save(businessmanInfo);
+    }
 
 }
