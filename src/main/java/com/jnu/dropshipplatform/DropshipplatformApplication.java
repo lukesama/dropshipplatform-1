7baab7be9d7e8e5a89c3fbdecbec8facf9329c80
guopeixin <peixin.guo24@gmail.com>
2018-07-25 11:17:41 +0800

    Initial commit

---
+++ src/main/java/com/jnu/dropshipplatform/DropshipplatformApplication.java
@@ -0,0 +1,12 @@
+package com.jnu.dropshipplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropshipplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropshipplatformApplication.class, args);
+    }
+}
---
+++ src/main/java/com/jnu/dropshipplatform/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.jnu.dropshipplatform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("jnu")
+public class TestController {
+
+    @GetMapping
+    public String test(){
+        return "index";
+    }
+
+}
