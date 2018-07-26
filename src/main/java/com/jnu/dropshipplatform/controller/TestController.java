191728a25a8f6e16c772dc1e9a8b2c4c15a2e264
= <=>
2018-07-26 22:07:54 +0800

    删除addBrandInfo页面的TODO（并未实现所需功能）

--- src/main/java/com/jnu/dropshipplatform/controller/TestController.java
+++ src/main/java/com/jnu/dropshipplatform/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.jnu.dropshipplatform.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @Controller
 @RequestMapping("jnu")
@@ -21,6 +25,11 @@ public class TestController {
         return "index";
     }
 
-
+//    @GetMapping("t")
+//    public String tester(Model model){
+//        String s ="abcd";
+//        model.addAttribute("test",s);
+//        return "ProductDetail";
+//    }
 
 }
